f3e07b8a8bdff975daf334b9694b04f3860f781e
Isabella Camilli Meletani <20241014040028@ifrn.local>
2025-10-08 08:32:17 -0300

    Exercicios em andamento

---
+++ exercicios-typecript/ex1.ts
@@ -0,0 +1,13 @@
+function calcularQuadrado1(arr:Array<number>) {
+    for (let i = 0; i < arr.length; i++){
+        let quadrado = i**2
+        console.log(quadrado)
+    }
+}
+
+function calcularQuadrado2(arr:Array<number>) {}
+
+
+var exArray = [3,5,7,3,8,9,1]
+
+console.log(calcularQuadrado1(exArray))
